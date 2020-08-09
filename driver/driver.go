69ce2fa98c96ae1ff9d61024163bef9b0cf0d3d7
yujialves <yujialves@i.softbank.jp>
2020-08-09 12:43:16 +0900

    Printlnの削除

--- driver/driver.go
+++ driver/driver.go
@@ -2,7 +2,6 @@ package driver
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
@@ -14,20 +13,15 @@ var db *sql.DB
 func ConnectDB() *sql.DB {
 
 	// DB 接続
-	fmt.Println(os.Getenv("USER"))
-	fmt.Println(os.Getenv("PASSWORD"))
-	fmt.Println(os.Getenv("HOSTNAME"))
-	fmt.Println(os.Getenv("DBNAME"))
 	db, err := sql.Open("mysql", os.Getenv("USER")+":"+os.Getenv("PASSWORD")+"@tcp("+os.Getenv("HOSTNAME")+")/"+os.Getenv("DBNAME"))
 	if err != nil {
-		// log.Fatal(err)
-		log.Println(err)
+		log.Fatal(err)
 	}
 
 	// DB 疎通確認
 	err = db.Ping()
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 
 	return db
